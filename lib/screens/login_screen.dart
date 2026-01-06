76706810577ea2ba588c56e518c4c3c208892378
HasanKaranouh <hkaranouh2004@gmail.com>
2026-01-06 16:59:08 +0200

    commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../services/api_service.dart';
 import 'register_screen.dart';
-import 'home_screen.dart';
+import 'attendance_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
